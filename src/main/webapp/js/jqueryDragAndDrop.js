b338214c1c1cce5469395fa1bbed14885242052d
Freakout2114 <harry2114spam@live.com>
2017-05-22 17:54:26 +1000

    Updated things for drag and drop

--- src/main/webapp/js/jqueryDragAndDrop.js
+++ src/main/webapp/js/jqueryDragAndDrop.js
@@ -27,28 +27,66 @@ function init() {
     // Add offers to the sidebar
     function addOffer( $item ) {
         var imageSrc = $item.context.childNodes[1].children[0].children[0].getAttribute("src");
-        var offerName = $item.context.childNodes[1].children[1].children[0].textContent
-        console.log( $item.context.childNodes[1].children[1].children[0].textContent );
+        var offerID = $item.context.getAttribute("id");
+        var offerName = $item.context.getAttribute("title");
+        var offerPrice = $item.context.getAttribute("price");
+        var onclick = "removeOffer(" + '"' + offerID + '"' + ")";
         
-        var newCard = $("<li class='ui-widget-content'>" + 
+        Materialize.toast(offerName + " Added!", 4000) // 4000 is the duration of the toast
+        
+        var newCard = $("<li id='" + offerID + "' title='" + offerName +"' price='" + offerPrice +"' class='ui-widget-content'>" + 
             "<div class='card col m12'>" +
+                "<a onclick='" + onclick + "' class='material-icons right'><img src='images/icons/cross.png'></a>" +
                 "<div class='card-image'>" +
-                    "<img class='col m6 activator' src='" + imageSrc + "'>" +
+                    "<img class='left col m6' src='" + imageSrc + "'>" +
                 "</div>" +
-                "<span class='card-title grey-text text-darken-4'>" + offerName + "</span>" +
-                "<a onclick='removeOffer(this)' class='material-icons right'><img src='images/icons/cross.png'></a>" +
+                "<span class='card-title grey-text text-darken-4'>" + offerName + " $" + offerPrice + "</span>" +
                 "</div>" +
             "</div>" +
         "</li>");
         
         $('#addedOffers').prepend(newCard);
+        newCard[0].setAttribute("offer", newCard[0]);
+        console.log( newCard[0] );
+        
+        updateCost();
         console.log("Added");
     }
 }
     
 function removeOffer( $item ) {
     console.log("removeOffer()");
-    $('#addedOffers').splice($item, 1);
-    console.log($item);
+    
+    var list = $('#addedOffers')[0].children;
+    
+    for (var i = 0; i < list.length; i++) {
+        if (list[i].id == $item) {
+            list[i].remove();
+            i = list.length; 
+        }
+    }
+        
+    updateCost();
     console.log("Removed");
 }
+
+function updateCost() {
+    console.log("updateCost()");
+    
+    var list = $('#addedOffers')[0].children;
+    var cost = 0;
+    
+    for (var i = 0; i < list.length; i++) 
+        cost += new Number(list[i].getAttribute("price"));
+    
+    // Set the price
+    $('#totalPrice')[0].textContent = "Total: $" + cost;
+    $('#totalSavings')[0].textContent = "Savings: $" + cost*0.2;
+    
+    console.log("Updated");
+}
+
+
+
+
+
